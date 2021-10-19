94f82db3ae58e150678cc449d93c3908d8ca955e
Tobias Høj-Kristensen <Tobias.Hoj-Kristensen@egekom.dk>
2021-10-19 14:54:23 +0200

    Jeg har nu færdiggjort strategyPattern

---
+++ Bygninger/StrategyPattern/DatabaseStrategy/SQL.cs
@@ -0,0 +1,15 @@
+﻿using StrategyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.DatabaseTypes
+{
+    class SQL : IDatabase
+    {
+        public void database()
+        {
+            Console.WriteLine("Jeg er en lokaldatabase - god til store steder med egen infrastruktur");
+        }
+    }
+}
